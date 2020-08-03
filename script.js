58db140a4ee1f2f315537e12942535d281b5579f
Vova <volodiabodnarchyk2000@gmail.com>
2020-08-03 21:16:12 +0300

    Task_3

--- script.js
+++ script.js
@@ -22,10 +22,20 @@ const a = prompt('Один из последних просмотренных ф
 //Task 2
       if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
         personalMovieDB.movies[a] = b;
-        console.log(done);
+        console.log('done');
       } else {
           console.log ('ERror');
           i--;
     }
 };
-    
+
+if (personalMovieDB.count < 10) {
+    console.log('Переглянута дуже мала кількість фільмів!');
+} else if (10 < personalMovieDB.count < 30) {
+    console.log('Ви Любитель кіно!:)');
+} else if (personalMovieDB.count >= 30 ) {
+    console.log('Ви кіноман!');
+} else {
+    console.log('Виникла помилка!');
+}
+console.log(personalMovieDB);
