b2dc60335c80e5e7fdac5f98ba3cfed0362697ed
Vova <volodiabodnarchyk2000@gmail.com>
2020-08-03 21:07:24 +0300

    Task_1 and Task_2 2

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 "use strike";
 
-//Task 1
+
 const numberOfFilms = +prompt('Скільки фільмів ви уже побачили?','');
 
 
-//Task 2
+
 const  personalMovieDB = {
    count : numberOfFilms,
     movies : {},
@@ -13,13 +13,13 @@ const  personalMovieDB = {
       privat : false
    };
 
-//Task 3
+//Task 1
 
 for (let i = 1; i < 2; i++){
 
 const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-
+      b = prompt('На сколько оцените его?', '');
+//Task 2
       if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
         personalMovieDB.movies[a] = b;
         console.log(done);
