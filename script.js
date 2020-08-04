4066106320adcb7ce775708aee06b4fa4890d4e8
Vova <volodiabodnarchyk2000@gmail.com>
2020-08-04 19:12:55 +0300

    Start

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-"use strike";
+// "use strike";
 
 
-const numberOfFilms = +prompt('Скільки фільмів ви уже побачили?','');
+// const numberOfFilms = +prompt('Скільки фільмів ви уже побачили?','');
 
 
 
-const  personalMovieDB = {
-   count : numberOfFilms,
-    movies : {},
-     actors : {},
-      genres : [],
-      privat : false
-   };
+// const  personalMovieDB = {
+//    count : numberOfFilms,
+//     movies : {},
+//      actors : {},
+//       genres : [],
+//       privat : false
+//    };
 
-//Task 1
+// //Task 1
 
-for (let i = 1; i < 2; i++){
+// for (let i = 1; i < 2; i++){
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', '');
-//Task 2
-      if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-      } else {
-          console.log ('ERror');
-          i--;
-    }
-};
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', '');
+// //Task 2
+//       if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//       } else {
+//           console.log ('ERror');
+//           i--;
+//     }
+// };
 
-if (personalMovieDB.count < 10) {
-    console.log('Переглянута дуже мала кількість фільмів!');
-} else if (personalMovieDB.count < 10 && personalMovieDB.count > 30 ) {
-    console.log('Ви Любитель кіно!:)');
-} else if (personalMovieDB.count >= 30 ) {
-    console.log('Ви кіноман!');
-} else {
-    console.log('Виникла помилка!');
-}
-console.log(personalMovieDB);
+// if (personalMovieDB.count < 10) {
+//     console.log('Переглянута дуже мала кількість фільмів!');
+// } else if (personalMovieDB.count < 10 && personalMovieDB.count > 30 ) {
+//     console.log('Ви Любитель кіно!:)');
+// } else if (personalMovieDB.count >= 30 ) {
+//     console.log('Ви кіноман!');
+// } else {
+//     console.log('Виникла помилка!');
+// }
+// console.log(personalMovieDB);
